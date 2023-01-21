7b07b613e2074b58494a7091f488d5d6ef675775
Brian Hofmeister <brianmhofmeister@gmail.com>
2023-01-20 21:05:17 -0600

    Page updates...

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Playing With NextJS</span>,
+  logo: <span>TechCheats</span>,
   project: {
     link: 'https://github.com/brianmhofmeister/documentation-starter-kit',
   },
